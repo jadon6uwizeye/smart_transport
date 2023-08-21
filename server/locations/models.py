b7f16620c12a5fc1bd13b3bcac51cd1a1987bbce
jadon6uwizeye <jeandedieuuwizeye6@gmail.com>
2023-08-21 14:58:09 +0200

    finish the back-end setup

--- server/locations/models.py
+++ server/locations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics', null=True, blank=True)
+    desc = models.TextField()
+    price = models.IntegerField(null=True, blank=True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()

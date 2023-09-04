31caeb99b8480f943eb8c362cc42863e3ec0bf6c
jadon6uwizeye <jeandedieuuwizeye6@gmail.com>
2023-09-04 19:41:42 +0200

    progress as on 04-09

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:app/presentation/outgoing.dart';
+import 'package:app/presentation/views/login.dart';
+import 'package:app/presentation/views/outgoing.dart';
 import 'package:app/presentation/views/locationDisplay.dart';
 import 'package:app/presentation/views/locations.dart';
 import 'package:flutter/material.dart';
@@ -27,86 +28,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Location and Passengers Display'),
-          backgroundColor: Color.fromARGB(255, 3, 10, 20),
-          // change the app bar height
-          toolbarHeight: 70,
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 4, 37, 99),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Color.fromARGB(255, 4, 37, 99),
         ),
-        // body: Center(
-        //   child: LocationDisplay(
-        //     locations: [
-        //       LocationInfo(
-        //         location: LatLng(-1.951344, 30.089222),
-        //         name: 'Kagugu',
-        //         passengers: ['UWIZEYE Jean de Die', 'Kamali Jean'],
-        //       ),
-        //       LocationInfo(
-        //         location: LatLng(-1.952092, 30.096263),
-        //         name: 'Kinamba',
-        //         passengers: ['King Ngabo', 'Kamali Jean'],
-        //       ),
-        //       LocationInfo(
-        //         location: LatLng(-1.953092, 30.090263),
-        //         name: 'Kinamba',
-        //         passengers: ['King Ngabo', 'Kamali Jean'],
-        //       ),
-        //       LocationInfo(
-        //         location: LatLng(-1.953002, 30.096263),
-        //         name: 'Kinamba',
-        //         passengers: ['King Ngabo', 'Kamali Jean'],
-        //       )
-        //       // Add more locations as needed
-        //     ],
-        //   ),
-        // ),
+        focusColor: Color.fromARGB(255, 4, 37, 99),
+      ),
+      home: SplashScreen(),
+      // Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Ticket Tracking app'),
+      //     backgroundColor: Color.fromARGB(255, 3, 10, 20),
+      //     // change the app bar height
+      //     toolbarHeight: 70,
+      //   ),
+      //   // body: Center(
+      //   //   child: LocationDisplay(
+      //   //     locations: [
+      //   //       LocationInfo(
+      //   //         location: LatLng(-1.951344, 30.089222),
+      //   //         name: 'Kagugu',
+      //   //         passengers: ['UWIZEYE Jean de Die', 'Kamali Jean'],
+      //   //       ),
+      //   //       LocationInfo(
+      //   //         location: LatLng(-1.952092, 30.096263),
+      //   //         name: 'Kinamba',
+      //   //         passengers: ['King Ngabo', 'Kamali Jean'],
+      //   //       ),
+      //   //       LocationInfo(
+      //   //         location: LatLng(-1.953092, 30.090263),
+      //   //         name: 'Kinamba',
+      //   //         passengers: ['King Ngabo', 'Kamali Jean'],
+      //   //       ),
+      //   //       LocationInfo(
+      //   //         location: LatLng(-1.953002, 30.096263),
+      //   //         name: 'Kinamba',
+      //   //         passengers: ['King Ngabo', 'Kamali Jean'],
+      //   //       )
+      //   //       // Add more locations as needed
+      //   //     ],
+      //   //   ),
+      //   // ),
 
-        body: PassengersToGetOffList(
-          locations: [
-            "Kagugu",
-            "Kinamba",
-            "Kacyiru",
-            "Kanombe",
-            "Kimironko",
+      //   body: LoginScreen(),
+      // ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate some initialization work (e.g., fetching data, loading resources)
+    Future.delayed(Duration(seconds: 3), () {
+      // Navigate to the main screen or the desired screen after the splash screen
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(
+          255, 4, 37, 99), // Background color of the splash screen
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Ticket Tracking App', // Replace with your app's name
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white, // Text color
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                  Colors.white), // Loading indicator color
+            ),
           ],
-          passengers: {
-            "Kagugu": [
-              "UWIZEYE Jean de Dieu",
-              "Kamali Jean",
-              "King Ngabo",
-              "John Doe",
-              "Jane Doe",
-              "John",
-              "Jane",
-              "Rwibutso"
-            ],
-            "Kinamba": [
-              "King Ngabo",
-              "James K",
-              "King Ngabo",
-              "Rwibutso Jean",
-              "King Ngabo",
-              "Yves Jean",
-              "King Ngabo",
-              "Kamali Jean",
-              "Kim Ngabo",
-              "Kamali Jean",
-              "King Ngabo",
-              "Kamali Jean",
-              "King Ngabo",
-              "Kanam Jean",
-              "King Ngabo",
-              "Kamali Jean",
-              "XYZ Ngabo",
-              "Name Jean",
-              "King Name",
-              "Kamali Names",
-            ],
-            "Kacyiru": ["King Ngabo", "Kamali Jean"],
-            "Kanombe": ["King Ngabo", "Kamali Jean"],
-            "Kimironko": ["King Ngabo", "Kamali Jean"],
-          },
         ),
       ),
     );

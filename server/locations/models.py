96f92fa15dd1152ff30d52001c9100eed6517a12
jadon6uwizeye <jeandedieuuwizeye6@gmail.com>
2023-09-12 14:41:33 +0200

    changes

--- server/locations/models.py
+++ server/locations/models.py
@@ -14,12 +14,18 @@ class Destination(models.Model):
         return self.name
 
 class Ticket(models.Model):
+    TICKET_STATUS = (
+        ('P', 'Pending'),
+        ('O', 'On-going'),
+        ('C', 'Completed'),
+    )
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     email = models.CharField(max_length=100, null=True, blank=True)
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
     date = models.DateField(auto_created=True)
     time = models.TimeField()
+    status = models.CharField(max_length=1, choices=TICKET_STATUS, default='P')
 
     def __str__(self):
         return self.name
